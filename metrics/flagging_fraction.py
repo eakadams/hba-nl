92f48ed9c35033369d6d8a8687f5e9cd7679d996
Betsey Adams <eakadams@gmail.com>
2025-04-28 09:54:37 +0200

    Start flagging metric exploration

---
+++ metrics/flagging_fraction.py
@@ -0,0 +1,148 @@
+# Flagging fractions metric
+
+__author__ = "E. A. K. Adams"
+
+"""
+Examine flagging fraction as a possible metric
+
+Start with calibrators
+"""
+
+# import modules
+import json
+import statistics
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define some global paths
+cal_json_file_path = '../data/cal_json'  # hope this relative path works
+
+
+def get_cal_from_json(json_path):
+    """Get calibrator information from json
+    Don't just return the dictionary but drill down to a semi-useful level
+
+    Returns field_name (string) and station_data (dictionary)
+    """
+    with open(json_path) as json_file:
+        json_data = json.load(json_file)
+
+    # drill down to a semi-useful level
+    cal_data = json_data['metrics']['LINC']
+    # close_sources = cal_data['close_sources']  # will maybe want this later
+    field_name = cal_data['field_name']
+    station_data = cal_data['stations']
+
+    return field_name, station_data
+
+
+def get_flagging_single_cal(json_path):
+    """
+    Load data from json, using helper function
+    Get flagging fractions, this is for single calibrator
+    :param json_path: Path to calibrator json file, string
+    :return:
+    """
+    field_name, station_data = get_cal_from_json(json_path)
+    # iterate through stations
+    # want to store info by station_type
+    flagged_data_core = []
+    flagged_data_remote = []
+    flagged_data_intl = []
+    for s in station_data:
+        # check station type
+        station = s['station']
+        if station[0:2] == 'CS':
+            station_type = 'core'
+        elif station[0:2] =='RS':
+            station_type = 'remote'
+        else:
+            station_type = 'intl'
+        # get flagged data
+        flagged_frac = s['percentage_flagged']['final']
+        # add to correct array
+        if station_type == 'core':
+            flagged_data_core.append(flagged_frac)
+        elif station_type == 'remote':
+            flagged_data_remote.append(flagged_frac)
+        else:
+            flagged_data_intl.append(flagged_frac)
+    # return arrays of flagged data fractions
+    # then can work with them elsewhere to explore metrics
+    return flagged_data_core, flagged_data_remote, flagged_data_intl
+
+
+def get_flagging_frac_metric(json_path):
+    """For a single calibrator observation
+    with output json given by json_path, get flagging metrics
+    Put this in separate function so can easily update
+    """
+    # first get arrays of flagged fractions
+    (flagged_data_core, flagged_data_remote,
+     flagged_data_intl) = get_flagging_single_cal(json_path)
+
+    # now get some metrics
+    # place in a dictionary
+    flagged_fracs = {}
+    flagged_fracs['core'] = {'median': statistics.median(flagged_data_core),
+                             'mean': statistics.fmean(flagged_data_core)}
+    print(flagged_fracs)
+    flagged_fracs['remote'] = {'median': statistics.median(flagged_data_remote),
+                               'mean': statistics.fmean(flagged_data_remote)}
+    flagged_fracs['international'] = {'median': statistics.median(flagged_data_intl),
+                                      'mean': statistics.fmean(flagged_data_intl)}
+    print(flagged_fracs)
+
+    # and return the dictionary
+    return flagged_fracs
+
+
+def find_all_cals(cal_path=cal_json_file_path):
+    """
+    Search for the cal directory structure and create a list of all file paths.
+    This will be used later for pulling statistics of everything to investigate metrics
+    Also print an overall summary of how many cal files are available
+    and how many for each calibrator
+    :param cal_path:
+    :return:
+    """
+    json_file_list = glob.glob(os.path.join(cal_path, 'L*/*calibrator_summary.json'))
+    file_name_list = [f.split('/')[-1] for f in json_file_list]
+    cal_name_list = [n.split('_')[0] for n in file_name_list]
+    names, count = np.unique(cal_name_list, return_counts=True)
+    print(f"The calibrators and their count are:")
+    print([f"{c}: {n}" for c, n in zip(names, count)])
+    print(f"The total number of calibrator json files is {len(json_file_list)}")
+    # return the json_file_list with full paths
+    return json_file_list
+
+
+def examine_global_flagging_metrics(cal_path=cal_json_file_path):
+    """
+    Take a path to all the calibrators, get the file list (other function),
+    iterate through to get flagging stats (other function),
+    and then plot up the results
+    Also return in arrays/lists so can work with further
+    :param cal_path:
+    :return:
+    """
+    json_file_list = find_all_cals(cal_path)
+    # create a dictionary to put things in
+    # want to keep structure and also ability to sort by type
+    global_flags = []
+    for f in json_file_list:
+        ff = get_flagging_frac_metric(f)
+        obs = f.split('/')[-2]
+        cal = f.split('/')[-1].split('_')[0]
+        cal_dict = {'Observation ID': obs, 'Calibrator': cal,
+                    'Flagging fractions': ff}
+        global_flags.append(cal_dict)
+
+
+
+
+
+
+
