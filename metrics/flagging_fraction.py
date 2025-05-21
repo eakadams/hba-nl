78c94628ea4524e39053d450b01eb98b5a5c04bb
Betsey Adams <eakadams@gmail.com>
2025-05-21 17:54:01 +0200

    Look at good stations and start saving text output

--- metrics/flagging_fraction.py
+++ metrics/flagging_fraction.py
@@ -15,6 +15,8 @@ import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.table import Table
+from astropy.io import ascii
 
 # define some global paths
 # do this in a hacky way
@@ -93,29 +95,32 @@ def get_flagging_frac_metric(json_path, threshold=70):
     try:
         flagged_fracs['core'] = {'median': statistics.median(flagged_data_core),
                                  'mean': statistics.fmean(flagged_data_core),
-                                 'n_thresh': sum(i > threshold for i in flagged_data_core),
+                                 'n_thresh': sum(i >= threshold for i in flagged_data_core),
+                                 'n_pass': sum(i < threshold for i in flagged_data_core),
                                  'n_tot': len(flagged_data_core)}
     except:
         flagged_fracs['core'] = {'median': np.nan, 'mean': np.nan,
-                                 'n_thresh': np.nan, 'n_tot': np.nan}
+                                 'n_thresh': np.nan, 'n_tot': np.nan, 'n_pass': np.nan}
     try:
         flagged_fracs['remote'] = {'median': statistics.median(flagged_data_remote),
                                    'mean': statistics.fmean(flagged_data_remote),
-                                   'n_thresh': sum(i > threshold for i in flagged_data_remote),
+                                   'n_thresh': sum(i >= threshold for i in flagged_data_remote),
+                                   'n_pass': sum(i < threshold for i in flagged_data_remote),
                                    'n_tot': len(flagged_data_remote)
                                    }
     except:
         flagged_fracs['remote'] = {'median': np.nan, 'mean': np.nan,
-                                   'n_thresh': np.nan, 'n_tot': np.nan}
+                                   'n_thresh': np.nan, 'n_tot': np.nan, 'n_pass': np.nan}
     try:
         flagged_fracs['international'] = {'median': statistics.median(flagged_data_intl),
                                           'mean': statistics.fmean(flagged_data_intl),
-                                          'n_thresh': sum(i > threshold for i in flagged_data_intl),
+                                          'n_thresh': sum(i >= threshold for i in flagged_data_intl),
+                                          'n_pass': sum(i < threshold for i in flagged_data_intl),
                                           'n_tot': len(flagged_data_intl)
                                           }
     except:
         flagged_fracs['international'] = {'median': np.nan, 'mean': np.nan,
-                                          'n_thresh': np.nan, 'n_tot': np.nan}
+                                          'n_thresh': np.nan, 'n_tot': np.nan, 'n_pass': np.nan}
 
     # and return the dictionary
     return flagged_fracs
@@ -141,13 +146,21 @@ def find_all_cals(cal_path=cal_json_file_path):
     return json_file_list
 
 
-def examine_global_flagging_metrics(cal_path=cal_json_file_path, threshold=70):
+def examine_global_flagging_metrics(cal_path=cal_json_file_path, threshold=70,
+                                    output='flagged_data_cals',
+                                    n_pass_core=40, n_pass_remote=10,
+                                    n_pass_intl=10):
     """
     Take a path to all the calibrators, get the file list (other function),
     iterate through to get flagging stats (other function),
     and then plot up the results
     Also return in arrays/lists so can work with further
-    :param cal_path:
+    :param n_pass_intl: Number of international stations required to pass
+    :param n_pass_remote: Number of remote stations required to pass
+    :param n_pass_core: Number of core stations required to pass
+    :param threshold: number, percentage data flagged threshold, default=70
+    :param output: filename (no extension) for output image and table
+    :param cal_path: path of all calibrator json files
     :return:
     """
     json_file_list = find_all_cals(cal_path)
@@ -159,15 +172,21 @@ def examine_global_flagging_metrics(cal_path=cal_json_file_path, threshold=70):
     fd_c_mean = []
     fd_c_median = []
     fd_c_thresh = []
+    fd_c_pass = []
     fd_c_tot = []
     fd_r_mean = []
     fd_r_median = []
     fd_r_thresh = []
     fd_r_tot = []
+    fd_r_pass = []
     fd_i_mean = []
     fd_i_median = []
     fd_i_thresh = []
     fd_i_tot = []
+    fd_i_pass = []
+    core_pass = []
+    remote_pass = []
+    intl_pass = []
     for f in json_file_list:
         ff = get_flagging_frac_metric(f, threshold=threshold)
         obs_list.append(f.split('/')[-2])
@@ -178,14 +197,34 @@ def examine_global_flagging_metrics(cal_path=cal_json_file_path, threshold=70):
         fd_c_median.append(ff['core']['median'])
         fd_c_thresh.append(ff['core']['n_thresh'])
         fd_c_tot.append(ff['core']['n_tot'])
+        fd_c_pass.append(ff['core']['n_pass'])
         fd_r_mean.append(ff['remote']['mean'])
         fd_r_median.append(ff['remote']['median'])
         fd_r_thresh.append(ff['remote']['n_thresh'])
         fd_r_tot.append(ff['remote']['n_tot'])
+        fd_r_pass.append(ff['remote']['n_pass'])
         fd_i_mean.append(ff['international']['mean'])
         fd_i_median.append(ff['international']['median'])
         fd_i_thresh.append(ff['international']['n_thresh'])
         fd_i_tot.append(ff['international']['n_tot'])
+        fd_i_pass.append(ff['international']['n_pass'])
+        # check and store if things pass
+        core_pass.append(True) if ff['core']['n_pass'] >= n_pass_core else core_pass.append(False)
+        remote_pass.append(True) if ff['remote']['n_pass'] >= n_pass_remote else remote_pass.append(False)
+        intl_pass.append(True) if ff['international']['n_pass'] >= n_pass_intl else intl_pass.append(False)
+
+    # get an overall table to output
+    metric_table = Table([obs_list, core_pass, remote_pass, intl_pass,
+                          fd_c_pass, fd_r_pass, fd_i_pass],
+                         names=('ObsID','Core Pass', 'Remote Pass', 'Intl Pass',
+                                'Nstation good core', 'Nstation good remote',
+                                'Nstation good intl'))
+    metric_table.meta['comments'] = [f'Threshold of flagging is {threshold}',
+                                     f"{n_pass_core} core stations to pass",
+                                     f"{n_pass_remote} remote stations to pass",
+                                     f"{n_pass_intl} international stations to pass"]
+    metric_table.write(f'{output}.csv', format='csv', overwrite=True)
+
 
     # now visualize things
     # plot histograms for everything
@@ -210,32 +249,39 @@ def examine_global_flagging_metrics(cal_path=cal_json_file_path, threshold=70):
     ax3.legend()
     ax4.scatter(fd_c_median, np.array(fd_c_thresh)/np.array(fd_c_tot), s=3)
     ax4.plot([threshold, threshold], [0,1], color='orange')
-    ax4.set_ylabel('Fraction above threshold')
+    ax4.plot([0, 100], [0.5, 0.5], color='orange', linestyle='--')
+    ax4.set_ylabel('Fraction of stations above threshold')
     ax4.set_xlabel('Median flagged fraction')
     ax4.set_title(f'Core Threshold of {threshold}')
     ax5.scatter(fd_r_median, np.array(fd_r_thresh)/np.array(fd_r_tot), s=3)
     ax5.plot([threshold, threshold], [0, 1], color='orange')
-    ax5.set_ylabel('Fraction above threshold')
+    ax5.plot([0, 100], [0.5, 0.5], color='orange', linestyle='--')
+    ax5.set_ylabel('Fraction of stations above threshold')
     ax5.set_xlabel('Median flagged fraction')
     ax5.set_title(f'Remote Threshold of {threshold}')
     ax6.scatter(fd_i_median,np.array(fd_i_thresh)/np.array(fd_i_tot), s=3)
     ax6.plot([threshold, threshold], [0, 1], color='orange')
-    ax6.set_ylabel('Fraction above threshold')
+    ax6.plot([0, 100], [0.5, 0.5], color='orange', linestyle='--')
+    ax6.set_ylabel('Fraction of stations above threshold')
     ax6.set_xlabel('Median flagged fraction')
     ax6.set_title(f'Intl Threshold of {threshold}')
-    ax7.scatter(fd_c_median, fd_c_thresh, s=3)
+    # Look at numbers that pass
+    ax7.scatter(fd_c_median, fd_c_pass, s=3)
     ax7.plot([threshold, threshold], [0, 50], color='orange')
-    ax7.set_ylabel('Number above threshold')
+    ax7.plot([0, 100], [n_pass_core, n_pass_core], color='orange', linestyle='--')
+    ax7.set_ylabel('Number of stations passing')
     ax7.set_xlabel('Median flagged fraction')
-    ax8.scatter(fd_r_median, fd_r_thresh, s=3)
+    ax8.scatter(fd_r_median, fd_r_pass, s=3)
     ax8.plot([threshold, threshold], [0, 15], color='orange')
-    ax8.set_ylabel('Number above threshold')
+    ax8.plot([0, 100], [n_pass_remote, n_pass_remote], color='orange', linestyle='--')
+    ax8.set_ylabel('Number of stations passing')
     ax8.set_xlabel('Median flagged fraction')
-    ax9.scatter(fd_i_median, fd_i_thresh, s=3)
+    ax9.scatter(fd_i_median, fd_i_pass, s=3)
     ax9.plot([threshold, threshold], [0, 15], color='orange')
-    ax9.set_ylabel('Number above threshold')
+    ax9.plot([0, 100], [n_pass_intl, n_pass_intl], color='orange', linestyle='--')
+    ax9.set_ylabel('Number of stations passing')
     ax9.set_xlabel('Median flagged fraction')
-    plt.savefig('flagged_data_cals.pdf')
+    plt.savefig(f'{output}.pdf')
 
 
 
